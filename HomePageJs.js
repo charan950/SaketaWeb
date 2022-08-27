5d238faf6a4d14766a2c908ef12d8c9d427ab235
Sai Charan Rendedla <saicharan.rendedla@gmail.com>
2022-08-27 23:47:54 +0530

    UI Change

--- HomePageJs.js
+++ HomePageJs.js
@@ -1,3 +1,20 @@
+let itcount=0;
+let hrcount=0;
+let mdcount=0;
+let salescount=0;
+let seatlecount=0;
+let indiacount=0;
+let netcount=0;
+let sharepointcount=0;
+let recount=0;
+let bicount=0;
+let bacount=0;
+let necount=0;
+let pmcount=0;
+let omcount=0;
+let uicount=0;
+let secount=0;
+let talentcount=0;
 var alphastore=`
 <button class ="grid "><i class="fa fa-user"></i></button>
 `;
@@ -29,10 +46,17 @@ function addEmployee(){
     var skypeid=document.getElementById("skypeid");
 
     var emp=new employeeDetails("Image1.jfif",fname,lname,email,job,title,office,department,pnum,skypeid);
-    empArray.push(emp);
-    empList();
-    document.getElementById("empList").style.display="";
-    document.getElementById("form").style.display = "none";
+    if(fname==""||lname=='' || email==''|| job=='' || title=='' || office=='' || department==''|| pnum==''|| skypeid==''){
+        alert("Please Fill all details")
+    }
+    else{
+        empArray.push(emp);
+        empList();
+        alert("Employee Details Added")
+        document.getElementById("empList").style.display="";
+        document.getElementById("add-form").style.display = "none";
+    }
+   
      
 }
 function openForm() {
@@ -40,6 +64,7 @@ function openForm() {
     document.getElementById("empList").style.display="none";
   }
 function employeeDetails(img,fname,lname,email,job ,title,office,department,pnum,skypeid){
+
     this.img=img;
     this.fname=fname;
     this.lname=lname;
@@ -50,14 +75,10 @@ function employeeDetails(img,fname,lname,email,job ,title,office,department,pnum
     this.department=department;
     this.pnum=pnum;
     this.skypeid=skypeid;
-    // employeeDetails.prototype.fullname = function() {
-    //     return this.fname + " " + this.lname;
-    //   };
-
 }
 
 
-let emp1=new employeeDetails("Image1.jfif","Antony","Morris","antony@gmail.com","SharePoint Practice Head","SharePoint Practice Head","Seattle","IT Department","123456789","antony@skype");
+let emp1=new employeeDetails("Image1.jfif","Antony","Morris","antony@gmail.com","SharePoint Practice Head","SharePoint Practice Head","Seatle","IT Department","123456789","antony@skype");
 let emp2=new employeeDetails("Image2.jfif","Helen","Zimmerman","helen@gmail.com","Operations Manager","Operations Manager","India","HR Department",123456789,"helen@skype");
 let emp3=new employeeDetails("Image3.jfif","Jonathon","Smith","smith@gmail.com","Product Manager","Product Manager","India","UX Department",123456789,"smith@skype");
 let emp4=new employeeDetails("Image4.jfif","Tami","Hopkins","tami@gmail.com","Lead Enginer-Dot Net","Lead Enginer-Dot Net","India","IT Department",123456789,"tami@skype");
@@ -80,30 +101,9 @@ function empList(){
     let templist="";
     for(var i=0;i<empArray.length;i++){
        
-       
-        const list = `
-        
-        <div class="list" >
-        <div onclick='getDetails(${i})'>
-            <div class="row" >
-                <div class="col-4 ">
-                    <img class="profile p-2 " src="${empArray[i].img}">
-                </div>
-            <div class="col p-0">
-                <div>${empArray[i].fname} ${empArray[i].lname}</div>
-                <div>${empArray[i].job}</div>
-                <div>${empArray[i].department}</div>
-                <i><img class="icon" src="call-grey.png"></i>
-                <i><img class="icon" src="popup.png"></i>
-                <i><img class="icon" src="message.png"></i>
-                <i><img class="icon" src="heart.png"></i>
-                <i><img class="icon" src="star.png"></i>
-            </div>
-            </div>
-        </div>
-    </div>              
-    `;
-    templist =templist+list;
+        filters(i)
+    
+    templist =templist+ filters(i);
     }
     var alpha=document.getElementById('empList');
     alpha.innerHTML=templist;
@@ -118,15 +118,15 @@ function getDetails(v){
             <img src="${empArray[v].img}" alt="Avatar" class="avatar">
           </div>
           <div class="container p-5 text-center">
-          Firstname:${empArray[v].fname}<br>
-          last Name:${empArray[v].lname}<br><br>
-          Email:${empArray[v].email}<br><br>
-          Job:${empArray[v].job}<br><br>
-          title: ${empArray[v].title}<br><br>
-          Office: ${empArray[v].office}<br><br>
-          department:${empArray[v].department}><br><br>
-          Phone Number: ${empArray[v].pnum}<br><br>
-          SkypeId :${empArray[v].skypeid}<br><br>
+            <p><b>Firstname:-</b>${empArray[v].fname}</p>
+            <p><b> last Name:-</b>${empArray[v].lname}</p>
+            <p><b>Email:-</b>${empArray[v].email}</p>
+            <p><b>Job:-${empArray[v].job}</p>
+            <p><b>title:- </b>${empArray[v].title}</p>
+            <p><b> Office:-</b> ${empArray[v].office}</p>
+            <p><b> department:-</b>${empArray[v].department}</p>
+            <p><b>Phone Number:-</b> ${empArray[v].pnum}</p>
+            <p><b>SkypeId :${empArray[v].skypeid}</p>
             <button type="button" onclick="popInfo(${v})">Update</button>
           </div>
         </form>
@@ -155,30 +155,34 @@ function popInfo(v){
         <img src="${empArray[v].img}" alt="Avatar" class="avatar">
       </div>
       <div class="container p-5 ">
-      <label><b>First Name: </b></label>
-      <input type="text" class="w-100" id="fname" value="${empArray[v].fname}"><br><br>
-      <label><b>Last Name</b></label>
-      <input type="text" class="w-100"id="lname" value="${empArray[v].lname}"><br><br>
-      <label><b>Emaill Address</b></label>
-      <input type="text" class="w-100" id="email" value="${empArray[v].email}"><br><br>
-      <label><b>Job</b></label>
-      <input type="text"class="w-100" id="job" value="${empArray[v].job}"><br><br>
-      <label><b>Title</b></label>
-      <input type="text" class="w-100" id="title"value="${empArray[v].title}"><br><br>
-      <label><b>Office</b></label>
-      <input type="text" class="w-100"  id="office" value="${empArray[v].office}"><br><br>
-      <label><b>Department</b></label>
-      <input type="text" class="w-100" id="department" value="${empArray[v].department}"><br><br>
-      <label><b>Phone Number</b></label>
-      <input type="text" class="w-100" id="pnum" value="${empArray[v].pnum}"><br><br>
-      <label><b>Skype Id</b></label>
-      <input type="text" class="w-100" id="skypeid" value="${empArray[v].skypeid}"><br><br>
-      <button type="button" onclick="onSubmit(${v})">Submit</button>
+        <div class="row">
+            <div class="col">
+            <label><b>First Name: </b></label>
+            <input type="text" class="w-100" id="fname" value="${empArray[v].fname}"><br><br>
+            <label><b>Last Name</b></label>
+            <input type="text" class="w-100"id="lname" value="${empArray[v].lname}"><br><br>
+            <label><b>Emaill Address</b></label>
+            <input type="text" class="w-100" id="email" value="${empArray[v].email}"><br><br>
+            <label><b>Job</b></label>
+            <input type="text"class="w-100" id="job" value="${empArray[v].job}"><br><br>
+            <label><b>Title</b></label>
+            <input type="text" class="w-100" id="title"value="${empArray[v].title}"><br><br>
+            </div>
+            <div class="col">
+            <label><b>Office</b></label>
+            <input type="text" class="w-100"  id="office" value="${empArray[v].office}"><br><br>
+            <label><b>Department</b></label>
+            <input type="text" class="w-100" id="department" value="${empArray[v].department}"><br><br>
+            <label><b>Phone Number</b></label>
+            <input type="text" class="w-100" id="pnum" value="${empArray[v].pnum}"><br><br>
+            <label><b>Skype Id</b></label>
+            <input type="text" class="w-100" id="skypeid" value="${empArray[v].skypeid}"><br><br>
+            <button type="button" onclick="onSubmit(${v})">Submit</button>
+            </div>
+        </div>
     </div>
     </form>
   </div>
-       
-    
     `;
     var alpha=document.getElementById('empList');
     alpha.innerHTML=le;
@@ -190,39 +194,17 @@ function popInfo(v){
 
 
 function searchBox(){
-    let val=document.getElementById('preferd').value.toLowerCase();
+    let val=document.getElementById('search').value.toLowerCase();
     renderEmployeesBySearch(val);
 }
 
 const renderEmployeesBySearch = (inputStr) => {
-   filtearray=["fullname",]
+  
    var temp="";
     for(var i=0;i<empArray.length;i++){
         if(empArray[i].department.toLowerCase().includes(inputStr)||empArray[i].fname.toLowerCase().includes(inputStr)|| empArray[i].job.toLowerCase().includes(inputStr)){
-
-            const list = `
-        
-            <div class="list">
-            <div onclick='popInfo(${i})'>
-                <div class="row">
-                    <div class="col-4 ">
-                        <img class="profile p-2 " src="Image8.jfif">
-                    </div>
-                <div class="col p-0">
-                    <div>${empArray[i].fname} ${empArray[i].lname}</div>
-                    <div>${empArray[i].job}</div>
-                    <div>${empArray[i].department}</div>
-                    <i><img class="icon" src="call-grey.png"></i>
-                    <i><img class="icon" src="popup.png"></i>
-                    <i><img class="icon" src="message.png"></i>
-                    <i><img class="icon" src="heart.png"></i>
-                    <i><img class="icon" src="star.png"></i>
-                </div>
-            </div>
-            </div>
-        </div>              
-        `;
-        temp =temp+list;
+            filters(i)
+            temp =temp+ filters(i);
         } 
     }
     var alpha=document.getElementById('empList');
@@ -264,34 +246,12 @@ function GetDataURL()
 }
 
 function getDepartment(val) {
-    console.log(val);
+   
   var departmentstrore="";
     for(var i=0;i<empArray.length;i++){
         if(empArray[i].department.toLowerCase().includes(val.toLowerCase())){
-
-            const list = `
-        
-            <div class="list">
-            <div onclick='popInfo(${i})'>
-                <div class="row">
-                    <div class="col-4 ">
-                        <img class="profile p-2 " src="${empArray[i].img}">
-                    </div>
-                <div class="col p-0">
-                    <div>${empArray[i].fname} ${empArray[i].lname}</div>
-                    <div>${empArray[i].job}</div>
-                    <div>${empArray[i].department}</div>
-                    <i><img class="icon" src="call-grey.png"></i>
-                    <i><img class="icon" src="popup.png"></i>
-                    <i><img class="icon" src="message.png"></i>
-                    <i><img class="icon" src="heart.png"></i>
-                    <i><img class="icon" src="star.png"></i>
-                </div>
-            </div>
-            </div>
-        </div>              
-        `;
-        departmentstrore =departmentstrore+list;
+            filters(i)
+            departmentstrore =departmentstrore+ filters(i);
         }
         
     }
@@ -304,30 +264,9 @@ function getOffice(val) {
   var officestrore="";
     for(var i=0;i<empArray.length;i++){
         if(empArray[i].office.toLowerCase().includes(val.toLowerCase())){
-
-            const list = `
-        
-            <div class="list">
-            <div onclick='popInfo(${i})'>
-                <div class="row">
-                    <div class="col-4 ">
-                        <img class="profile p-2 " src="${empArray[i].img}">
-                    </div>
-                <div class="col p-0">
-                    <div>${empArray[i].fname} ${empArray[i].lname}</div>
-                    <div>${empArray[i].job}</div>
-                    <div>${empArray[i].department}</div>
-                    <i><img class="icon" src="call-grey.png"></i>
-                    <i><img class="icon" src="popup.png"></i>
-                    <i><img class="icon" src="message.png"></i>
-                    <i><img class="icon" src="heart.png"></i>
-                    <i><img class="icon" src="star.png"></i>
-                </div>
-            </div>
-            </div>
-        </div>              
-        `;
-        officestrore =officestrore+list;
+            filters(i)
+       
+        officestrore =officestrore+ filters(i);
         }
         
     }
@@ -339,33 +278,124 @@ function getJob(val) {
     var jobstore="";
       for(var i=0;i<empArray.length;i++){
           if(empArray[i].job.toLowerCase().includes(val.toLowerCase())){
-  
-              const list = `
-          
-              <div class="list">
-              <div onclick='popInfo(${i})'>
-                  <div class="row">
-                      <div class="col-4 ">
-                          <img class="profile p-2 " src="${empArray[i].img}">
-                      </div>
-                  <div class="col p-0">
-                      <div>${empArray[i].fname} ${empArray[i].lname}</div>
-                      <div>${empArray[i].job}</div>
-                      <div>${empArray[i].department}</div>
-                      <i><img class="icon" src="call-grey.png"></i>
-                      <i><img class="icon" src="popup.png"></i>
-                      <i><img class="icon" src="message.png"></i>
-                      <i><img class="icon" src="heart.png"></i>
-                      <i><img class="icon" src="star.png"></i>
-                  </div>
-              </div>
-              </div>
-          </div>              
-          `;
-          var jobstore=jobstore+list;
+            filters(i);
+            var jobstore=jobstore+filters(i);
           }
           
       }
       var alpha=document.getElementById('empList');
           alpha.innerHTML=jobstore;
+       
   }
+  
+  for(var i=0;i<empArray.length;i++){
+  
+        if(empArray[i].department.includes('IT')){
+            itcount++;
+        }
+        if(empArray[i].department.includes('HR')){
+            hrcount++;
+        }
+        if(empArray[i].department.includes('MD')){
+        mdcount++;
+        }
+        if(empArray[i].department.includes("Sales")){
+            salescount++;
+        }
+        if(empArray[i].job.includes('SharePoint Practice Head')){
+            sharepointcount++;
+        }
+        if(empArray[i].job.includes('Dot Net Lead')){
+            netcount++;
+        }
+        if(empArray[i].job.includes('Recruiting Expert')){
+                recount++;
+        }
+        if(empArray[i].office.toLowerCase().includes('seatle')){
+            seatlecount++;
+            }
+        if(empArray[i].office.includes('India')){
+            indiacount++;
+        }
+       
+        if(empArray[i].job=="Operations Manager"){
+            omcount=omcount+1;
+            console.log(omcount);
+        }
+        if(empArray[i].job==("Product Manager")){
+            pmcount++;
+        }
+        if(empArray[i].job==("Network Engineer")){
+            necount++;
+        }
+        if(empArray[i].job==("UI Designer")){
+            uicount++;
+        }
+        if(empArray[i].job=="Software Engineer"){
+            secount++;
+        }
+        if(empArray[i].job=="Talent Magnet Jr."){
+            talentcount++;
+        }
+        if(empArray[i].job=="UX Designer"){
+            uicount++;
+        }
+  }
+  
+  document.getElementById("itcount").innerHTML = itcount;
+  document.getElementById("hrcount").innerHTML=hrcount;
+  document.getElementById("mdcount").innerHTML= mdcount;
+  document.getElementById("salescount").innerHTML= salescount;
+  document.getElementById("sharepointcount").innerHTML= sharepointcount;
+  document.getElementById("netcount").innerHTML= netcount;
+  document.getElementById("recount").innerHTML= recount;
+  document.getElementById("seatlecount").innerHTML= seatlecount;
+  document.getElementById("indiacount").innerHTML= indiacount;
+  document.getElementById("bacount").innerHTML= bacount;
+  document.getElementById("bicount").innerHTML= bicount;
+
+function viewMore(){
+   
+    const view=`
+        <div id='om'> Operations Manger(<span id="omcount">${omcount}</span>)</div>
+        <div id="pm">Product Manager(<span id="pmcount">${pmcount}</span>)</div>
+        <div id='se'>Software Engineer(<span id="secount">${secount}</span>)</div>
+        <div id='ne'>Network Engineer(<span id="necount">${necount}</span>)</div>
+        <div id="talent">Talent Magnent Jr(<span id="talentcount">${talentcount}</span>)</div>
+        <div id='ui'>UI Designer(<span id="uicount">${uicount}</span>)</div>
+    `;
+
+    let more=document.getElementById('viewmore');
+    more.innerHTML=view;
+    document.getElementById('more').style.display="none";
+}
+
+function preferred(id){
+    var l=document.getElementById('se').value;
+    console.log(l);
+}
+function filters(index){
+    const list = `
+          
+    <div class="list">
+    <div onclick='popInfo(${index})'>
+        <div class="row">
+            <div class="col-4 ">
+                <img class="profile p-2 " src="${empArray[index].img}">
+            </div>
+        <div class="col p-0">
+            <div>${empArray[index].fname} ${empArray[index].lname}</div>
+            <div>${empArray[index].job}</div>
+            <div>${empArray[index].department}</div>
+            <i><img class="icon" src="call-grey.png"></i>
+            <i><img class="icon" src="popup.png"></i>
+            <i><img class="icon" src="message.png"></i>
+            <i><img class="icon" src="heart.png"></i>
+            <i><img class="icon" src="star.png"></i>
+        </div>
+    </div>
+    </div>
+</div>              
+`;
+return list;
+}
